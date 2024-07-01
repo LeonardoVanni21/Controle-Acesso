07753bbf360faba1e85e8a7f462ef9b2ec7925e5
Leonardo Vanni Bonavigo <leonardovanni21@gmail.com>
2024-07-01 20:11:57 -0300

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,74 @@
 # ControleAcesso
+
+## Descrição
+
+Aplicação para simular um controle de acesso de usuários com permissões.
+
+## Tecnologias
+
+- Python
+- PostgreSQL
+- Flask (Framework Python)
+
+## Instalação
+pip install flask psycopg2-binary
+
+## Configuração
+1. Criar um banco de dados no PostgreSQL nome "BaseControleAcesso"
+2. Executar o script: 
+```bash
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE permissions (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) UNIQUE NOT NULL
+);
+
+CREATE TABLE user_permissions (
+    user_id INT REFERENCES users(id),
+    permission_id INT REFERENCES permissions(id),
+    PRIMARY KEY (user_id, permission_id)
+);
+```
+
+## Inserir dados
+```bash
+INSERT INTO users (username, password) VALUES 
+('admin', 'Test1234'),
+('user1', 'Test123');
+
+INSERT INTO permissions (name) VALUES 
+('teste_tela_comum'),
+('teste_link_admin');
+
+INSERT INTO user_permissions (user_id, permission_id) VALUES 
+(1, 1),
+(1, 2),
+(2, 1);
+```
+
+## Executar
+```bash
+python app.py
+```
+
+## Acessar
+http://127.0.0.1:5000/login
+
+Login: admin
+
+Senha: Test1234
+
+* Nesse caso, o usuário admin tem permissão para ver o link da tela comum e o link da tela de admin.
+
+Login: user1
+
+Senha: Test123
+
+* Nesse caso, o usuário user1 tem permissão para ver apenas o link da tela comum.
+
  
---
+++ app.py
@@ -0,0 +1,65 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+import psycopg2
+from psycopg2 import sql
+
+app = Flask(__name__)
+app.secret_key = 'secretKey123'
+
+conn = psycopg2.connect(
+    dbname="BaseControleAcesso",
+    user="postgres",
+    password="postgres",
+    host="localhost",
+    port="5432"
+)
+
+
+def get_user_permissions(user_id):
+    with conn.cursor() as cur:
+        cur.execute("""
+            SELECT p.name 
+            FROM permissions p
+            JOIN user_permissions up ON p.id = up.permission_id
+            WHERE up.user_id = %s
+        """, (user_id,))
+        return [row[0] for row in cur.fetchall()]
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        with conn.cursor() as cur:
+            cur.execute("SELECT id FROM users WHERE username = %s AND password = %s", (username, password))
+            user = cur.fetchone()
+
+            if user:
+                session['user_id'] = user[0]
+                return redirect(url_for('dashboard'))
+            else:
+                return "Dados Incorretos", 403
+
+    return render_template('login.html')
+
+
+@app.route('/dashboard')
+def dashboard():
+    if 'user_id' not in session:
+        return redirect(url_for('login'))
+
+    user_id = session['user_id']
+    permissions = get_user_permissions(user_id)
+
+    return render_template('dashboard.html', permissions=permissions)
+
+
+@app.route('/logout')
+def logout():
+    session.pop('user_id', None)
+    return redirect(url_for('login'))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
